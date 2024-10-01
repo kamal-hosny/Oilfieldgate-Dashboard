f1b961a0cb3a701c1f86705a8f49d259c89c7d57
kamal <kamal@gmail.com>
2024-10-01 03:08:14 +0300

    sss

--- src/components/modalDialog/LogOut.jsx
+++ src/components/modalDialog/LogOut.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { closeModal } from '../../store/modal/modalSlice';
+import { logout } from '../../store/login/loginAuthSlice';
 
 const LogOut = () => {
     const navigate = useNavigate();
@@ -9,6 +10,7 @@ const LogOut = () => {
 
     const confirmLog = () => {
         dispatch(closeModal());
+        logout()
         navigate("./login", { replace: true })
     }
 
@@ -32,7 +34,7 @@ const LogOut = () => {
             {/* S btn */}
             <div className="col-span-2 flex justify-between gap-2 mt-2 items-center">
                 <div className="flex items-center gap-2">
-                    <button className="bg-green-500 text-white px-3 py-2 rounded-sm" onClick={confirmLog}>
+                    <button className="bg-red-700 hover:bg-red-900 text-white px-3 py-2 rounded-sm" onClick={confirmLog}>
                     Leave
                     </button>
                     <button onClick={cancel} className="bg-gray-500 text-white px-3 py-2 rounded-sm">
